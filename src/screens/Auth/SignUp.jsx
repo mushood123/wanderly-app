1e5f3b6d0baca663f3bd9fa42134f595c7dc2151
Khawaja Muhammad Mushood <muhammad.mushood@A006-00467.local>
2024-07-25 18:36:16 +0500

    install firebase and it's configuration for android and ios

---
+++ src/screens/Auth/SignUp.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>Sign_up</Text>
+    </View>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({});
